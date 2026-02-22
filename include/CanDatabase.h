26c8684bc4e2a8d0618eff23127678b120be31bb
Cole Abbott <caskisail18@icloud.com>
2026-02-22 15:03:52 -0600

    testing

--- include/CanDatabase.h
+++ include/CanDatabase.h
@@ -11,11 +11,13 @@ typedef struct {
 
 constexpr CanMessageDef ENGINE_RPM = {0x100, FLOAT, 4};
 constexpr CanMessageDef INT_DATA = {0x101, INT32, 4};
+constexpr CanMessageDef INT8_DATA = {0x102, UINT8, 1};
 
 
 constexpr CanMessageDef canDatabase[] = {
     ENGINE_RPM,
-    INT_DATA
+    INT_DATA,
+    INT8_DATA
 };
 
 constexpr size_t canDatabaseSize = sizeof(canDatabase) / sizeof(canDatabase[0]);
