77a4977fd4cade91f3eb5e19d89d77e1545a0d6f
AlexanderHalley <alexanderhalley2023@u.northwestern.edu>
2026-04-09 20:31:56 -0500

    Update CAN message definitions and IDs
    Added all the different messages from the spreadsheet and used a new ID system

--- include/CanDatabase.h
+++ include/CanDatabase.h
@@ -12,14 +12,19 @@ typedef struct
 namespace CanDatabase
 {
 
-    // 1xx data for HUD
-    // 2xx data for debugging and tuning, not sent to HUD
-    // 3xx arbitrary messages for testing
+    // All CAN IDs follow this scheme, first three bits in binary represent sender (100) = ECVT, (010) = DAQ, (001) = HUD.
+    //Next three bits follow the same scheme but for receiver (more than one receiver possible) and then last 5 bits are message ids (32 possibilities)
 
-    constexpr CanMessageDef ENGINE_RPM = {0x100, FLOAT, sizeof(float)};
-    constexpr CanMessageDef SECONDARY_RPM = {0x101, FLOAT, sizeof(float)};
-
-    constexpr CanMessageDef MOTOR_SETPOINT = {0x200, INT32, sizeof(int32_t)};
+    constexpr CanMessageDef ENGINE_RPM = {0x441, FLOAT, sizeof(float)};
+    constexpr CanMessageDef SECONDARY_RPM = {0x442, FLOAT, sizeof(float)};
+    constexpr CanMessageDef MODE = {0x443, INT32, sizeof(uint32_t)};
+    constexpr CanMessageDef STATUS = {0x444, INT32, sizeof(uint32_t)};
+    constexpr CanMessageDef TEMPERATURE = {0x445, FLOAT, sizeof(float)};
+    constexpr CanMessageDef FUEL_LEVEL = {0x222, FLOAT, sizeof(float)};
+    constexpr CanMessageDef LINEAR_SPEED = {0x221, FLOAT, sizeof(float)};
+    constexpr CanMessageDef BRAKE_POT = {0x141, FLOAT, sizeof(float)};
+    constexpr CanMessageDef FRONT_WHEELS = {0x142, FLOAT, sizeof(float)};
+    constexpr CanMessageDef MOTOR_SETPOINT = {0x446, INT32, sizeof(int32_t)};
 
     constexpr CanMessageDef INT_DATA = {0x300, INT32, sizeof(int32_t)};
     constexpr CanMessageDef INT8_DATA = {0x301, UINT8, 1};
@@ -27,6 +32,13 @@ namespace CanDatabase
     constexpr CanMessageDef canDatabase[] = {
         ENGINE_RPM,
         SECONDARY_RPM,
+        MODE,
+        STATUS,
+        TEMPERATURE,
+        FUEL_LEVEL,
+        LINEAR_SPEED,
+        BRAKE_POT,
+        FRONT_WHEELS,
         MOTOR_SETPOINT,
         INT_DATA,
         INT8_DATA};
