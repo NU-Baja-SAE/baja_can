affb6e42902918a6e295939ecdb42698374e4acf
Cole Abbott <caskisail18@icloud.com>
2026-04-19 13:23:28 -0500

    updated can database

--- include/CanDatabase.h
+++ include/CanDatabase.h
@@ -9,17 +9,30 @@ typedef struct
     uint8_t dlc;
 } CanMessageDef;
 
+
 namespace CanDatabase
 {
 
-    // 1xx data for HUD
-    // 2xx data for debugging and tuning, not sent to HUD
-    // 3xx arbitrary messages for testing
+    // All CAN IDs follow this scheme, first three bits in binary represent sender (100) = ECVT, (010) = DAQ, (001) = HUD. Next three bits follow the same scheme but for receiver (more than one receiver possible) and then last 5 bits are message ids (32 possibilities)
 
-    constexpr CanMessageDef ENGINE_RPM = {0x100, FLOAT, sizeof(float)};
-    constexpr CanMessageDef SECONDARY_RPM = {0x101, FLOAT, sizeof(float)};
+    constexpr CanMessageDef ENGINE_RPM = {0x441, FLOAT, sizeof(float)};
+    constexpr CanMessageDef SECONDARY_RPM = {0x442, FLOAT, sizeof(float)};
+    constexpr CanMessageDef MODE = {0x443, UINT8, sizeof(uint8_t)};
+    constexpr CanMessageDef STATUS = {0x444, UINT8, sizeof(uint8_t)};
+    constexpr CanMessageDef ECVT_TEMP = {0x445, FLOAT, sizeof(float)};
+    constexpr CanMessageDef MOTOR_SETPOINT = {0x446, INT32, sizeof(int32_t)};
+    
+    constexpr CanMessageDef FUEL_LEVEL = {0x222, FLOAT, sizeof(float)};
+    constexpr CanMessageDef LINEAR_SPEED = {0x221, FLOAT, sizeof(float)};
+    
+    constexpr CanMessageDef BRAKE_POT = {0x141, FLOAT, sizeof(float)};
+    constexpr CanMessageDef FRONT_LEFT_WHEEL_SPEED = {0x142, FLOAT, sizeof(float)};
+    constexpr CanMessageDef FRONT_RIGHT_WHEEL_SPEED = {0x143, FLOAT, sizeof(float)};
 
-    constexpr CanMessageDef MOTOR_SETPOINT = {0x200, INT32, sizeof(int32_t)};
+
+
+
+    
 
     constexpr CanMessageDef INT_DATA = {0x300, INT32, sizeof(int32_t)};
     constexpr CanMessageDef INT8_DATA = {0x301, UINT8, 1};
